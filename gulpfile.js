b358edbbf5853233282df5d53967257441a63ebe
omonk <ollie@olliemonk.com>
2016-02-04 23:18:15 +0000

    test update

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,42 @@
-
 var gp = require('gulp-load-plugins')(),
-	gulp = require('gulp')
+    gulp = require('gulp'),
+    postcss = require('gulp-postcss'),
+    scss = require('postcss-scss'),
+    reporter = require('postcss-reporter'),
+    stylelint = require('stylelint')
+
+// Nice to have but not necessary for a small project like this
+gulp.task('scsslint', function() {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(gp.plumber({
+            errorHandler: gp.notify.onError('Error: <%= error.message %>')
+        }))
+        .pipe(postcss([
+            stylelint(),
+            reporter({
+                clearMessages: true
+            })
+        ], {
+            syntax: scss
+        }))
+})
 
 gulp.task('sass', function() {
-	return gulp.src('./scss/style.scss')
-	.pipe(gp.plumber({errorHandler: gp.notify.onError('Error: <%= error.message %>')}))
-	.pipe(gp.sass({
-		errLogToConsole: true
-	}))
-	.pipe(gp.autoprefixer())
-	.pipe(gulp.dest('./'))
+    return gulp.src('./scss/style.scss')
+        .pipe(gp.plumber({
+            errorHandler: gp.notify.onError('Error: <%= error.message %>')
+        }))
+        .pipe(gp.sass({
+            errLogToConsole: true
+        }))
+        .pipe(gp.autoprefixer())
+        .pipe(gulp.dest('./css/'))
 })
 
 gulp.task('default', function(callback) {
 
-	watching = true
-	gulp.watch("./scss/**/*.scss", ['sass'])
-	gulp.watch('./fonts/**/*', ['fonts'])
-	gulp.watch('./img/**/*', ['images'])
+    watching = true
+    gulp.watch("./scss/**/*.scss", ['sass'])
+    gulp.watch('./fonts/**/*', ['fonts'])
+    gulp.watch('./img/**/*', ['images'])
 })
---
+++ js/index.js
@@ -0,0 +1,68 @@
+var $ = jQuery;
+var map;
+$(document).ready(function() {
+
+    var easyPanel = $('#scotch-panel').scotchPanel({
+        containerSelector: 'body',
+        direction: 'right',
+        duration: 300,
+        distanceX: '50%',
+        enableEscapeKey: true
+    });
+
+    $('.toggle-panel').click(function() {
+        console.log('fired');
+        easyPanel.toggle();
+        return false;
+    });
+
+    var position = $.getJSON('http://ipinfo.io', function(data) {
+        pos = data.loc;
+    }).success(function() {
+
+        var i = pos.split(',');
+        var lat = i[0]
+        var lon = i[1]
+
+        var map = new GMaps({
+            div: '#map',
+            lat: lat,
+            lng: lon,
+        });
+
+        map.addMarker({
+            lat: lat,
+            lng: lon,
+        })
+
+        var weatherUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '35&lon=' + lon + '&appid=18c2d4d8af4cef4165b8fb0d389730ce&units=metric';
+
+        var weather = $.getJSON(weatherUrl, function(data) {
+        }).success(function(data) {
+            var temp = data.main.temp; // get temp
+            temp = Math.ceil(temp)
+
+
+            var cond = data.weather; // get conditions
+            var desc = cond[0].description; // take descriptions from condtions
+
+            var wind = data.wind;
+            var windSpeed = wind.speed;
+            windSpeed = Math.ceil(windSpeed);
+
+            $('#temp').append(temp + "&deg;C");
+            $('#cond').append(desc);
+            $('#wind').append(windSpeed + "mph");
+        })
+    });
+});
+
+
+// I had some trouble working out how to get the event listeners working
+// with the video tags... :( 
+function getVideos() {
+    var videos = document.getElementsByTagName('video');
+    console.log(videos);
+}
+
+getVideos();
